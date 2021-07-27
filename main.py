3dc2397c2cb9899e89f28f5f6b906f438ac35c06
HolyShaq <66175571+HolyShaq@users.noreply.github.com>
2021-07-27 21:01:35 +0800

    Added copy feature

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from tkinter import *
+import pyperclip
 
 
 def getMouse(event):
     coord = Label(frmCoords, text=f"X: {event.x_root}\tY: {event.y_root}")
-    labels.append(coord)
+    coordLabels.append(coord)
     coord.pack()
 
 
+def copy(event):
+    latest = coordLabels[-1].cget("text").split("\t")
+    latest = ", ".join([c[3:] for c in latest])
+    pyperclip.copy(latest)
+
+
 def clear(event):
-    for label in labels:
+    for label in coordLabels:
         label.pack_forget()
 
 
 root = Tk()
 frmControls = Frame(root)
-frmCoords= Frame(root)
-controlA = Label(frmControls, text="Q - Get Position")
+frmCoords = Frame(root)
+controlA = Label(frmControls, text="Z - Get Position")
 controlB = Label(frmControls, text="C - Copy Coordinates")
 controlC = Label(frmControls, text="X - Clear Window")
 controlA.pack()
@@ -25,7 +32,8 @@ controlC.pack()
 frmControls.grid(column=0, row=0)
 frmCoords.grid(column=1, row=0)
 
-labels = []
-root.bind("<q>", getMouse)
+coordLabels = []
+root.bind("<z>", getMouse)
+root.bind("<c>", copy)
 root.bind("<x>", clear)
 mainloop()
